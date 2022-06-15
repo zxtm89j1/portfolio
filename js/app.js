b567f4a37fc022e3d0fb673332f58cc68d88582e
zxtm89j1 <zxtm89j1@gmail.com>
2022-06-15 11:18:12 +0800

    second commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let texts = ['websites', 'connections', 'pancakes'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+console.log(texts[count])
+
+(function type() {
+
+    if(count === texts.length) {
+        count = 0;
+    }
+
+    currentText = texts[count];
+    letter = currentText.slice(0, index++);
+    
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 150);
+    
+}())
